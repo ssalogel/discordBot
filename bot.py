7fe95cba6163f8934e4b7908b604006171225241
ssalogel <charlestherien@hotmail.com>
2021-07-05 18:32:09 -0400

    create a basic bot (w/ !ping)

---
+++ bot.py
@@ -0,0 +1,39 @@
+import logging
+import discord
+
+from localconfig import token
+from discord.ext import commands
+
+logger = logging.getLogger("discord.bot")
+
+bot = commands.Bot(command_prefix="!", fetch_online_members=False, case_insensitive=True,
+                   activity=discord.Activity(type=discord.ActivityType.watching, name="for !help"))
+
+
+def start_logging():
+    syslogger = logging.getLogger('discord')
+    syslogger.setLevel(logging.WARNING)
+    handler = logging.FileHandler(filename="./logs/discordSys.log", encoding='utf-8', mode='a')
+    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+    handler.setFormatter(formatter)
+    syslogger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler(filename="./logs/discordClient.log", encoding='utf-8', mode='a')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to discord!')
+    start_logging()
+
+
+@bot.command(name="ping", description="pong!",
+             brief="pong!")
+async def ping(ctx: commands.Context):
+    await ctx.send("Pong!")
+
+
+if __name__ == '__main__':
+    bot.run(token)
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='discord-bot',
+    version='0.1.0',
+    install_requires=['discord.py'],
+    setup_requires=['flake8']
+)
